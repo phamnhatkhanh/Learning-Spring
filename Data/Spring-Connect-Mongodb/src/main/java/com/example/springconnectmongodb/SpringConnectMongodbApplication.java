fdd817c54e9fe41f61f3de869ccd2e05c71a053d
phamnhatkhanh <pnkhanh.hcm@vnpt.vn>
2023-12-14 13:47:14 +0700

    Project: refactor module data

---
+++ Data/Spring-Connect-Mongodb/src/main/java/com/example/springconnectmongodb/SpringConnectMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springconnectmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConnectMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConnectMongodbApplication.class, args);
+    }
+
+}
